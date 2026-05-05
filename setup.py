2ac84050292c20fbd3bc7008e94496d087903dc6
Mo Elbadawy <mohamed.asaad.elbadawy@gmail.com>
2026-05-05 18:18:28 +0300

    setup.py and install requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "WasteDetection",
+    version= '0.0.0' ,
+    author= "Mohamed Elbadawy",
+    author_email= "mohamed.asaad.elbadawy@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
